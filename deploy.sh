9a03376f863c61f2a29b2699901c5f6379425016
Iman Dezfuly <Iman_Dezfuly@racv.com.au>
2021-12-05 14:04:18 +1100

    fix(deploy): use new ssh port

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-ssh root@desphilman.com 'service dovecot stop'
-ssh root@desphilman.com 'service postfix stop'
+ssh -p 6222 root@desphilman.com 'service dovecot stop'
+ssh -p 6222 root@desphilman.com 'service postfix stop'
 
-scp -r ./dovecot root@desphilman.com:/etc
-scp -r ./postfix root@desphilman.com:/etc
+scp -P 6222 -r ./dovecot root@desphilman.com:/etc
+scp -P 6222 -r ./postfix root@desphilman.com:/etc
 
-ssh root@desphilman.com 'service dovecot start'
-ssh root@desphilman.com 'service postfix start'
+ssh -p 6222 root@desphilman.com 'service dovecot start'
+ssh -p 6222 root@desphilman.com 'service postfix start'
